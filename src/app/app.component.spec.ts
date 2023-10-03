471909b90a7d97ab71cc0f69e78bb8930b8f0a61
Rory O'Connor <gonzooo@gmail.com>
2023-10-03 17:07:53 +1000

    Initial push of servers component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,5 @@
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import {TestBed} from '@angular/core/testing';
+import {AppComponent} from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
@@ -15,7 +15,7 @@ describe('AppComponent', () => {
   it(`should have as title 'my--first--app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my--first--app');
+    // expect(app.name).toEqual('my--first--app');
   });
 
   it('should render title', () => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {ServerComponent} from './server/server.component';
+import {FormsModule} from '@angular/forms';
+import {ServersComponent} from './servers/servers.component';
+
+//import { HttpModule }  from '@angular/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+  //      HttpModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
